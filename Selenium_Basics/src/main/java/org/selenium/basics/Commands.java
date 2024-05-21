af4518d4c7ec3ef1ebc39e5f0b1418d23a1f2eec
nimmy1986 <148308970+nimmy1986@users.noreply.github.com>
2024-05-21 17:10:56 +0530

    DragandDrop

--- Selenium_Basics/src/main/java/org/selenium/basics/Commands.java
+++ Selenium_Basics/src/main/java/org/selenium/basics/Commands.java
@@ -1,13 +1,176 @@
 package org.selenium.basics;
 import java.util.List;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 
-public class Commands 
+public class Commands
 {
+	public void verifymousehover()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demoqa.com/menu/");
+		driver.manage().window().maximize();
+		
+		WebElement move2_option=driver.findElement(By.xpath("//a[text()='Main Item 2']"));
+		Actions action=new Actions(driver);
+		action.moveToElement(move2_option).build().perform();
+		
+		WebElement subset_option=driver.findElement(By.xpath("//a[text()='SUB SUB LIST »']"));
+		action.moveToElement(subset_option).build().perform();
+
+		driver.close();	
+	}
+	
+	public void verifydraganddropoffset()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demoqa.com/dragabble");
+		driver.manage().window().maximize();
+		
+		WebElement dragme=driver.findElement(By.xpath("//div[@id='dragBox']"));
+		
+		Actions action=new Actions(driver);
+		action.dragAndDropBy(dragme, 50, 55).build().perform();
+		driver.close();
+	}
+	public void verifydraganddrop()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demoqa.com/droppable");
+		driver.manage().window().maximize();
+		
+		WebElement dragme=driver.findElement(By.xpath("//div[@id='draggable']"));
+		WebElement dropdown=driver.findElement(By.xpath("//div[@id='simpleDropContainer']//div[@id='droppable']"));
+		Actions action=new Actions(driver);
+		action.dragAndDrop(dragme, dropdown).build().perform();
+		//driver.close();
+		
+	}
+	public void verifydoubliclickme()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		driver.manage().window().maximize();
+		
+		WebElement doubliclickme_button=driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
+		Actions action=new Actions(driver);
+		action.doubleClick(doubliclickme_button).build().perform();
+		
+		Alert alert=driver.switchTo().alert();
+		String alerttext=alert.getText();
+		System.out.println(alerttext);
+		alert.accept();
+		driver.close();
+		
+	}
+	public void verifymouserightclick()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+		driver.manage().window().maximize();
+		
+		WebElement rightclickme_button=driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+		Actions action=new Actions(driver);
+		action.contextClick(rightclickme_button).build().perform();
+		driver.close();
+	}
+	public void verifynoofdropdownvalues()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/newtours/register.php");
+		driver.manage().window().maximize();
+		
+		WebElement country_dropdown=driver.findElement(By.xpath("//select[@name='country']"));
+		Select selectobj=new Select(country_dropdown);
+		
+		List<WebElement> dropdownlist=selectobj.getOptions();
+		System.out.println("Total no of countries in the dropdown list: "+dropdownlist.size());
+	}
+	public void verifyvaluesfromdropdown()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.guru99.com/test/newtours/register.php");
+		driver.manage().window().maximize();
+		
+		WebElement country_dropdown=driver.findElement(By.xpath("//select[@name='country']"));
+		Select selectobj=new Select(country_dropdown);
+		//selectobj.selectByVisibleText("ANGOLA");
+		//selectobj.selectByIndex(8);
+		selectobj.selectByValue("BARBADOS");
+		WebElement firstoption=selectobj.getFirstSelectedOption();
+		String fo=firstoption.getText();
+		System.out.println(fo);
+		driver.close();
+	}
+	
+	public void verifymultipleoptionsfromdropdownlist()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://selenium.qabible.in/select-input.php");	
+		driver.manage().window().maximize();
+		
+		WebElement mutiselect=driver.findElement(By.xpath("//select[@id='multi-select-field']"));
+		Select selectobj=new Select(mutiselect);
+		boolean selectedoptions=selectobj.isMultiple();
+		System.out.println("Multiple values are selected: "+selectedoptions);
+		
+		selectobj.selectByIndex(2);
+		selectobj.selectByIndex(0);
+			List<WebElement> listofselectedoption =selectobj.getAllSelectedOptions();
+			for(WebElement e:listofselectedoption)
+			{
+				System.out.println(e.getText());
+			}
+			selectobj.deselectByIndex(0);
+		 } 
+	public void verifyisSelector()
+	{
+		boolean ismaleselected;
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/register");
+		driver.manage().window().maximize();
+		WebElement gender_male=driver.findElement(By.xpath("//input[@id='gender-male']"));
+		ismaleselected=gender_male.isSelected();
+		System.out.println("Male before selected: "+ismaleselected);
+		gender_male.click();
+		ismaleselected=gender_male.isSelected();
+		System.out.println("Male after selected: "+ismaleselected);
+		driver.close();
+		
+	}
+	public void verifyisEnabled()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+		
+		WebElement subscribe_button=driver.findElement(By.cssSelector("input#newsletter-subscribe-button"));
+		boolean isenabledornot=subscribe_button.isEnabled();
+		System.out.println("Subscribed button isd Enable"+isenabledornot);
+		subscribe_button.click();
+		isenabledornot=subscribe_button.isEnabled();
+		System.out.println("Subscribed button isd Enable"+isenabledornot);
+		
+	}
+
+	public void verifyisDisplayed()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.manage().window().maximize();
+		
+		WebElement vote_button=driver.findElement(By.xpath("//input[@id='vote-poll-1']"));
+		boolean isvotedisplayed=vote_button.isDisplayed();
+		System.out.println("Is the vote button displayed: "+isvotedisplayed);
+	}
+
 	public void verifydemowebshopRegister()
 	{
 		WebDriver driver=new ChromeDriver();
@@ -170,14 +333,7 @@ public class Commands
 	public static void main(String[] args) 
 	{
 		Commands cmdobj=new Commands();
-		//cmdobj.verifydemowebshop();
-		//cmdobj.verifySwagLabLogin();
-		//cmdobj.verifySwagLabLoginnew();
-		//cmdobj.verifydemowebshopLogin();
-		//cmdobj.verifypartialLinkText();
-		//cmdobj.verifynoftagsintheapplication();
-		//cmdobj.verifydemowebshopRegister();
-		cmdobj.verifyguru99registration();
+		cmdobj.verifymousehover();
 		
 		
 	}
